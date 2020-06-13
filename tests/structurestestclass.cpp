c33dfc3136ca6d9889943d5c56d31093d5d4a4a5
pi <pi@raspberrypi>
2020-06-14 00:04:25 +0200

    Stable commit before I try to implement a tree structure for PERFT

---
+++ tests/structurestestclass.cpp
@@ -0,0 +1,26 @@
+
+/*
+ * File:   structurestestclass.cpp
+ * Author: pi
+ *
+ * Created on Jun 13, 2020, 11:03:13 PM
+ */
+
+#include "structurestestclass.h"
+#include <string>
+#include <sstream>
+#include "structures.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(structurestestclass);
+
+structurestestclass::structurestestclass() {
+}
+
+structurestestclass::~structurestestclass() {
+}
+
+void structurestestclass::setUp() {
+}
+
+void structurestestclass::tearDown() {
+}
