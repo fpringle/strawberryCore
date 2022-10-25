16b979dc5d88a80e6dd430b0cc4abd30f75d8d8e
fpringle <freddyjepringle@gmail.com>
2022-10-25 12:57:38 +0200

    update copyright

--- typedefs.h
+++ typedefs.h
@@ -1,4 +1,10 @@
-/* Copyright 2020 Freddy Pringle */
+/*
+Copyright (c) 2022, Frederick Pringle
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
 #ifndef SRC_CORE_TYPEDEFS_H_
 #define SRC_CORE_TYPEDEFS_H_
 
